d923957044a267565c9bb1010e6757bd0d7f2528
MihaelaDanila100 <79402374+MihaelaDanila100@users.noreply.github.com>
2022-12-26 13:24:56 +0200

    created structure and added primeng

---
+++ frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IconButtonComponent } from './components/icon-button/icon-button.component';
+
+
+
+@NgModule({
+  declarations: [
+    IconButtonComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
