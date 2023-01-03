9573ba8f95803b6916ceebc079ae754b6df087b7
MihaelaDanila100 <79402374+MihaelaDanila100@users.noreply.github.com>
2023-01-03 21:45:03 +0200

    basic sign up

--- backend/auth.js
+++ backend/auth.js
@@ -39,3 +39,32 @@ module.exports.logIn = (connection, response, request) => {
         return response.status(404).json({message:"The user doesn't exist!"});
     });
 }
+
+module.exports.signUp = (connection, response, request) => {
+    if(request.accountType != 'candidates' && request.accountType != 'recruiters') {
+        response.status(400).json({message: "Bad request"});
+        throw TypeError("Bad request");
+    }
+    let table = request.accountType;
+    bcrypt.hash(request.password, 10, (err, hash) => {
+        if(err) {
+            response.status(500).json({message: "Something went wrong"});
+            throw Error(err);
+        }
+        let qr = '';
+        if(table === 'candidates') {
+            qr = `INSERT INTO ${table} (username, password, mail, fullName) VALUES ('${request.username}', '${hash}', '${request.mail}', '${request.fullName}')`;
+        } else if(table === 'recruiters') {
+            qr = `INSERT INTO ${table} (username, password, mail, fullname, company) VALUES ('${request.username}', '${hash}', '${request.mail}', '${request.fullName}', '${request.company}')`;
+        }
+        connection.query(qr, (err, result) => {
+            if(err) {
+                response.status(500).json({message: err});
+                throw Error(err);
+            }
+            response.status(200).json({userId: result.insertId});
+            return;
+        });
+    });
+
+}
