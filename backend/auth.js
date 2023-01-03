625d111c1e93f3183e6aaac299b01e2eedd0d96d
MihaelaDanila100 <79402374+MihaelaDanila100@users.noreply.github.com>
2023-01-03 21:15:38 +0200

    fixed issue

--- backend/auth.js
+++ backend/auth.js
@@ -2,42 +2,40 @@ const bcrypt = require("bcryptjs");
 
 module.exports.logIn = (connection, response, request) => {
     let password = request.password;
-    let unfound = true;
-    bcrypt.hash(password, 10, (err, hash) => {
+    bcrypt.hash(password, 10, async (err, hash) => {
         if(err) {
             response.status(500).json({message: "Something went wrong"});
             throw Error(err);
         }
         for(let table of ['candidates', 'recruiters']) {
             let qr = `SELECT * FROM ${table} WHERE username = '${request.username}'`;
-            connection.query(qr, (err, result) => {
-                if(err) {
-                    response.status(500).json({message: err});
-                    unfound = false;
-                    throw Error(err);
-                }
-                if(result.length > 0) {
-                    let res = result[0]
-                    bcrypt.compare(res.password, hash, function (err, isMatch) {
-                        if(err) {
-                            response.status(500).json({message: "Something went wrong"});
-                            unfound = false;
-                            throw Error(err);
-                        }
-                        if (isMatch){
-                            unfound = false;
-                            if(table === 'candidates') response.status(200).json({id: res.id_candidate, type: 'candidate'});
-                            else response.status(200).json({id: res.id_recruiter, type: 'recruiter'});
+            await new Promise((resolve, reject) => {
+                connection.query(qr, (err, result) => {
+                    if(err) {
+                        response.status(500).json({message: err});
+                        throw Error(err);
+                    }
+                    if(result.length > 0) {
+                        let res = result[0]
+                        bcrypt.compare(res.password, hash, function (err, isMatch) {
+                            if(err) {
+                                response.status(500).json({message: "Something went wrong"});
+                                throw Error(err);
+                            }
+                            if (isMatch){
+                                if(table === 'candidates') response.status(200).json({id: res.id_candidate, type: 'candidate'});
+                                else response.status(200).json({id: res.id_recruiter, type: 'recruiter'});
+                                return;
+                            } 
+                            response.status(200).json({message: "Wrong username or password"});
                             return;
-                        } 
-                        unfound = false;
-                        response.status(200).json({message: "Wrong username or password"});
-                        return;
-                    });
-                }
+                        });
+                    } else {
+                        resolve("done");
+                    }
+                });
             });
         }
-        if(unfound) return response.status(404).json({message:"The user doesn't exist!"});
-        return;
+        return response.status(404).json({message:"The user doesn't exist!"});
     });
 }
